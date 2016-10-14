d131fd808336895954d59ef0d17a19b77e7939dc
rnoctezuma <rnoctezuma@gmail.com>
2016-10-14 16:21:46 +0300

    upd12

---
+++ Task4/4.2. CustomSortDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2.CustomSortDemo
+{
+    internal class Program
+    {
+        private static bool Predicate(string x, string y)
+        {
+            if (x.Length > y.Length)
+                return false;
+            if (y.Length > x.Length)
+                return true;
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] < y[i])  //если x == y дописать проверку и вернуть true
+                    return true;
+            }
+            return false;
+        }
+
+        private static int Partition<T>(T[] array, int left, int right, Func<T, T, bool> Predicate)
+        {
+            int i = left;
+            for (int j = left; j <= right; j++)
+            {
+                if (Predicate(array[j], array[right]))
+                {
+                    T t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                    i++;
+                }
+            }
+            return i - 1;
+        }
+
+        private static void QuickSort<T>(T[] m, int left, int right, Func<T, T, bool> Predicate)
+        {
+            if (left >= right) return;
+            int c = Partition(m, left, right, Predicate);
+            QuickSort(m, left, c - 1, Predicate);
+            QuickSort(m, c + 1, right, Predicate);
+        }
+
+        private static void Main(string[] args)
+        {
+            string[] stringArray = { "aaqqq", "baqqq", "baqqq", "bbqqq", "Bbqqq", "Abqqq", "Aaqqq", "H", "ER", "bdy" };
+            QuickSort(stringArray, 0, stringArray.Length - 1, Predicate);
+            foreach (var item in stringArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Task4/4.3. SortingUnit/SortingUnit.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4._3.SortingUnit
+{
+    public class SortingUnit
+    {
+        public event EventHandler<EventArgs> SortComplete;
+
+        public static void SortFinished(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sort Finished");
+        }
+
+        public int Partition<T>(T[] numbers, int left, int right, Func<T, T, bool> Predicate)
+        {
+            T pivot = numbers[left];
+
+            while (true)
+            {
+                while (Predicate(pivot, numbers[left]))
+                    left++;
+
+                while (Predicate(numbers[right], pivot))
+                    right--;
+
+                if (left < right)
+                {
+                    T temp = numbers[right];
+                    numbers[right] = numbers[left];
+                    numbers[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+            SortComplete?.Invoke(this, new EventArgs());
+        }
+
+        public void QuickSort<T>(T[] arr, int left, int right, Func<T, T, bool> Predicate)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right, Predicate);
+
+                if (pivot > 1)
+                    QuickSort(arr, left, pivot - 1, Predicate);
+
+                if (pivot + 1 < right)
+                    QuickSort(arr, pivot + 1, right, Predicate);
+            }
+
+            //   if (right >= left)
+            //       SortComplete?.Invoke(this, new EventArgs());
+        }
+
+        public bool Predicate<T>(T first, T second)
+            where T : IComparable<T>
+        {
+            return (first.CompareTo(second) > 0);
+        }
+    }
+}
