9d37f61b60596db9d34cd32d22ed99da3df7b1b4
rnoctezuma <rnoctezuma@gmail.com>
2016-11-11 15:32:47 +0300

    task7

---
+++ RegexExpressions/Epam.RegexExpressions.HtmlReplacer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Epam.RegexExpressions.HtmlReplacer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"<[^<>]*>");
+
+            string text = "<ваек<<<<> укре <<sdfhd>> ик eergwf<>fd<<>b<>!<>";
+            Console.Write("Source text: {0}", text);
+            Console.WriteLine();
+            text = regex.Replace(text, "_");
+            Console.Write("Changed text: {0}", text);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RegexExpressions/RegexExpressions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExpressions
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"(((?:0[1-9]|1[0-9]|2[0-8])-(?:0[1-9]|1[0-2]))|((?:29|30|31)-(?:0[13578]|1[02]))|((?:29|30)-(?:0[469]|11))|((?:28|29)-02))-(?:[0-9]){4}");
+
+            string text = "tfe331-01-1996346паикецуbretg";
+
+            MatchCollection matches = regex.Matches(text);
+
+            Console.WriteLine($"Found matches: {matches.Count}");
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine($"Date: {match}");
+            }
+        }
+    }
+}
